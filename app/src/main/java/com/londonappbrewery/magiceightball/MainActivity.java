c8d204b29606bd5ae764b5c3c3159d930f2dd341
ManoharShrestha <manishamatya0814@gmail.com>
2018-06-05 22:43:44 +0545

    project completed!

--- app/src/main/java/com/londonappbrewery/magiceightball/MainActivity.java
+++ app/src/main/java/com/londonappbrewery/magiceightball/MainActivity.java
@@ -1,7 +1,14 @@
 package com.londonappbrewery.magiceightball;
 
+import android.provider.ContactsContract;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +16,35 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final Button askBtn = findViewById(R.id.ask_Btn);
+
+        final ImageView magicBall = findViewById(R.id.magic_Ball);
+
+        final int[] magic_array = {
+
+                R.drawable.ball13x,
+
+                R.drawable.ball23x,
+
+                R.drawable.ball33x,
+
+                R.drawable.ball43x,
+
+                R.drawable.ball53x
+        };
+
+        askBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d("magic","key pressed");
+                Random magicballgenerator = new Random();
+                int number = magicballgenerator.nextInt(5);
+                Log.d("magic","magic pressed" + number);
+                magicBall.setImageResource(magic_array[number]);
+
+
+            }
+        });
     }
 }
